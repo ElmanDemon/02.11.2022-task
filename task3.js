c98438e056ec4bb3d0016575245702a86cd89d0e
ElmanDemon <tu7lhkd9w@code.edu.az>
2022-11-02 02:42:01 +0400

    Homework

---
+++ task3.js
@@ -0,0 +1,38 @@
+var machine=[
+    {
+       
+        brand:"Mercedes",
+        model:"Benz",
+        color:"White",
+        year:"2017"
+    
+    },
+    {
+        brand:"Porsche",
+        model:"GTX",
+        color:"Red",
+        year:"2020"
+    },
+    {
+        brand:"BMW",
+        model:"x7",
+        color:"Black",
+        year:"2018"
+    },
+    {
+        brand:"Mercedes",
+        model:"C-Class",
+        color:"Metalic",
+        year:"2014"
+    },
+    {
+        brand:"Ford",
+        model:"Fussion",
+        color:"Blue",
+        year:"2017"
+    }
+]
+
+for(var i=0; i<machine.length; i++) {
+    console.log(machine[i].year);
+}
